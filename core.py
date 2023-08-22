e2d6b0048b48e2643580946d0ab18b17d7f3005d
steffyjk <steffy.jk2018@gmail.com>
2023-08-23 00:06:11 +0530

    FEAT:Added logic for fetching first APL1 card

--- core.py
+++ core.py
@@ -44,9 +44,68 @@ try:
     # Find all <a> elements
     # Currently working now @22.08.2023
     try:
-        a_elements = driver.find_elements(By.TAG_NAME, 'a')
-        for i in a_elements:
-            print(i.text)
+        region_name_elements = driver.find_elements(By.TAG_NAME, 'a')
+        region_names = [{i.text: []} for i in region_name_elements]
+        # print("This is the region names: ", region_names)
+        driver.find_element(By.TAG_NAME, 'a').click()
+        time.sleep(5)
+        sub_region_ele = driver.find_elements(By.TAG_NAME, 'a')
+        nex = []
+        for i in sub_region_ele:
+            nex.append(i.text)
+        # print("===>", nex)
+        driver.find_element(By.TAG_NAME, 'a').click()
+        time.sleep(3)
+        item_divs = driver.find_elements(By.CLASS_NAME, "dg_Item")
+        # for div in item_divs:
+        time.sleep(5)
+        div = item_divs[0]
+
+        td = div.find_elements(By.TAG_NAME, "td")
+        time.sleep(3)
+        # for i in td:
+            # print(i.text)
+        a_tag = div.find_elements(By.TAG_NAME, "a")
+        time.sleep(3)
+        apl_card = a_tag[1].click()
+        time.sleep(5)
+
+        card_lists = driver.find_elements(By.CLASS_NAME, "dg_Item")
+        time.sleep(3)
+        first_card = card_lists[0]
+        internal = first_card.find_elements(By.TAG_NAME, "td")
+        time.sleep(3)
+        final_data = {
+            "id": internal[0].text,
+            "card_holder_name": internal[1].text,
+            "hof_as_per_NFSA": internal[2].text,
+            "ration_card_no": internal[3].text,
+            "card_category": internal[4].text,
+            "family_member": internal[5].text,
+            "LPG_status": internal[6].text,
+            "PNG_status": internal[7].text,
+            "addr1": internal[8].text,
+            "village": internal[9].text,
+            "other_member": []
+        }
+        card_link = first_card.find_element(By.TAG_NAME, "a").click()
+        time.sleep(3)
+        members = driver.find_elements(By.CLASS_NAME, "dg_Item")
+        time.sleep(3)
+        for mem in members:
+            mem_detail = mem.find_elements(By.TAG_NAME, "td")
+            time.sleep(3)
+            final_data["other_member"].append({
+                "sr_no": mem_detail[0].text,
+                "member_name": mem_detail[1].text,
+                "age": mem_detail[2].text,
+                "gender": mem_detail[3].text,
+                "relation": mem_detail[4].text,
+            })
+
+
+
+
     except Exception as e:
         print("===>e", e)
 
